e7a757149899230ad95b7f77d875a693ce2f9929
kunal <ksikka@mun.ca>
2024-06-09 12:12:07 -0230

    comments table added

---
+++ routes/commnets.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const router = express.Router();
+
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  password: "176717",
+  database: "blog_website",
+  port: 5433,
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM comments");
+    res.json(rows);
+  } catch (err) {
+    console.error("Error getting comments:", err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.post("/create", async (req, res) => {
+  const { post_id, user_id, content } = req.body;
+  try {
+    const queryText =
+      "INSERT INTO comments (post_id, user_id, content) VALUES ($1, $2, $3) RETURNING *";
+    const { rows } = await pool.query(queryText, [post_id, user_id, content]);
+
+    res.status(201).json(rows[0]);
+  } catch (err) {
+    console.error("Error creating comment:", err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.post("/update/:commentid", async (req, res) => {
+  const { commentid } = req.params;
+  const { post_id, user_id, content } = req.body;
+  try {
+    // Update the comment in the database
+    const queryText =
+      "UPDATE comments SET post_id = $1, user_id = $2, content = $3 WHERE id = $4 RETURNING *";
+    const { rows } = await pool.query(queryText, [
+      post_id,
+      user_id,
+      content,
+      commentid,
+    ]);
+    if (rows.length === 0) {
+      return res.status(404).json({ message: "Comment not found" });
+    }
+    res.json(rows[0]);
+  } catch (err) {
+    console.error("Error updating comment:", err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.post("/delete/:commentid", async (req, res) => {
+  const { commentid } = req.params;
+  try {
+    const querytext = "DELETE FROM comments WHERE id = $1 RETURNING *";
+    const { rows } = await pool.query(querytext, [commentid]);
+    if (rows.length === 0) {
+      return res.status(404).json({ message: "Comment not found" });
+    }
+    res.json({ message: "Comment deleted successfully", post: rows[0] });
+  } catch (err) {
+    console.error("Error deleting comment:", err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+module.exports = router;
