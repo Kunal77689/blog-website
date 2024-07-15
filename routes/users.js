8da8ae29d432248c03659d16550116fa3a292422
kunal <ksikka@mun.ca>
2024-07-15 15:55:20 -0230

    added a new endpoint users

--- routes/users.js
+++ routes/users.js
@@ -133,6 +133,25 @@ router.get("/getUserId/:email", async (req, res) => {
   }
 });
 
+router.get("/getUserById/:userId", async (req, res) => {
+  let userId = req.params;
+  userId = userId.userId;
+  userId = parseInt(userId);
+  try {
+    const query = "SELECT username FROM users WHERE id = $1";
+    const result = await pool.query(query, [userId]);
+
+    if (result.rows.length > 0) {
+      res.status(200).json({ username: result.rows[0].username });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 router.post("/delete/:id", async (req, res) => {
   const { id } = req.params;
   try {
